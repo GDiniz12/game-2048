b17815a8a5fab78c7c5466b1d22fe13a2dd8ecf6
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2025-08-10 12:34:45 -0300

    Ajustei a lógica de geração de 2

--- script.js
+++ script.js
@@ -19,33 +19,47 @@ function fillContainer() {
     }
 }
 
+function square(r, c) {
+    const x2 = document.createElement("div");
+    x2.className = 'x2';
+    x2.innerText = "2";
+    document.getElementById(`${r}-${c}`).appendChild(x2);
+}
+
 function generate() {
-
-    if (!isEmpty()) {
-
-    }
-
     const result = isEmpty();
+    if (!result) {
+        return;
+    }
+    
     gameBoard[result.r][result.c] = 2;
+    square(result.r, result.c);
 }
 
 function isEmpty() {
+
+    let areEmpty = [];
     for (let r = 0; r < row; r++) {
         for (let c = 0; c < column; c++) {
             if (gameBoard[r][c] === 0) {
-                return { r, c };
-            } else {
-                return false;
+                areEmpty.push({ r, c });
             }
         }
     }
+
+    const raffle = Math.floor(Math.random() * areEmpty.length);
+    const result = areEmpty[raffle];
+    return result;
 }
 
 function slideRight() {
     for (let r = 0; r < row; r++) {
         for (let c = 0; c < column; c++) {
-            if (gameBoard[r][c] === gameBoard[r + 1][c]) {
-                gameBoard[r][c] += gameBoard[r + 1][c];
+            if (gameBoard[r][c] === gameBoard[r][c + 1] && gameBoard[r][c] !== 0) {
+                gameBoard[r][c] += gameBoard[r][c + 1];
+            } else if (gameBoard[r][c + 1] === 0 && gameBoard[r][c + 2] === 0) {
+                gameBoard[r][c] = gameBoard[3][c]
+                gameBoard[r][c] = 0;
             }
         }
     }
@@ -80,3 +94,9 @@ function slideDown() {
         }
     }
 }
+
+fillContainer()
+generate()
+generate()
+console.log(isEmpty())
+console.log(isEmpty())
